79b9898540e215479b2eae3a6a47932ebf2c67f8
MiniSharpy <46828188+MiniSharpy@users.noreply.github.com>
2024-08-22 03:35:50 +0100

    Switch to deducing this
    Not sure if this is better or not.
    Can avoid having to pass in the derived class and the subsequent casting to it, but it means there's a lot more repetitive typing unless auto is used.
    
    I'm not the biggest fan of auto because it can make things less obvious, but as the only times it's used here is on operators where you won't care to check the result, and on IdentityMatrix which is obvious, I've used it.

--- src/Maths/Matrix.h
+++ src/Maths/Matrix.h
@@ -40,13 +40,13 @@ namespace Engine3
 
 	namespace Detail
 	{
-		template <std::size_t RowSize, std::size_t ColumnSize, Number T, class Derived>
+		template <std::size_t RowSize, std::size_t ColumnSize, Number T>
 		struct MatrixBase : std::array<T, RowSize * ColumnSize>
 		{
 			/// @return A square matrix that's zeroed except for its diagonal elements, which are all one.
-			static constexpr Derived IdentityMatrix() requires (IsSquare<RowSize, ColumnSize>)
+			static constexpr auto IdentityMatrix() requires (IsSquare<RowSize, ColumnSize>)
 			{
-				Derived identityMatrix{};
+				Matrix<RowSize, ColumnSize, T> identityMatrix{};
 				for (std::size_t i = 0; i < RowSize; ++i) { identityMatrix[(RowSize + 1) * i] = 1; }
 
 				return identityMatrix;
@@ -54,7 +54,8 @@ namespace Engine3
 
 			/* Methods */
 			/// Flips a square matrix diagonally, in place. TODO: Should the function return a reference to this to allow chaining methods?
-			void Transpose() requires (RowSize == ColumnSize) { static_cast<Derived&>(*this) = this->Transposed(); }
+			template <typename Self>
+			void Transpose(this Self& self) requires (RowSize == ColumnSize) { self = self.Transposed(); }
 
 			/// Flips a matrix diagonally.
 			[[nodiscard]] Matrix<ColumnSize, RowSize, T> Transposed()
@@ -119,37 +120,43 @@ namespace Engine3
 			// Scalar multiplications.
 			/// Multiplies each element in the matrix by the scalar value \p other.
 			/// @return A reference to the now altered matrix for further operations.
-			constexpr Derived& operator*=(T rhs)
+			template <typename Self>
+			constexpr Self& operator*=(this Self& self, T rhs)
 			{
-				for (std::size_t i = 0; i < this->size(); ++i) { (*this)[i] *= rhs; }
-				return *static_cast<Derived*>(this);
+				for (std::size_t i = 0; i < self.size(); ++i) { self[i] *= rhs; }
+				return self;
 			}
 
 			/// Multiplies each element in the copied \p lhs by the scalar value \p rhs.
-			constexpr friend Derived operator*(Derived lhs, T rhs) { return lhs *= rhs; } // Intentional Matrix copy.
+			constexpr friend auto operator*(Matrix<RowSize, ColumnSize, T> lhs, T rhs) { return lhs *= rhs; }
+
+			// Intentional Matrix copy.
 
 			/// Multiplies each element in \p right by the scalar value \p left.
-			constexpr friend Derived operator*(T lhs, const Derived& rhs) { return rhs * lhs; }
+			constexpr friend auto operator*(T lhs, const Matrix<RowSize, ColumnSize, T>& rhs) { return rhs * lhs; }
 
 			/// Divides each element in the matrix by \p rhs in place.
 			/// @return A reference to the now altered matrix for further operations.
-			constexpr Derived& operator/=(T rhs)
+			template <typename Self>
+			constexpr Self& operator/=(this Self& self, T rhs)
 			{
-				for (std::size_t i = 0; i < this->size(); ++i) { (*this)[i] /= rhs; }
-				return static_cast<Derived&>(*this);
+				for (std::size_t i = 0; i < self.size(); ++i) { self[i] /= rhs; }
+				return self;
 			}
 
 			/// Divides each element in \p lhs by the scalar value \p rhs.
-			constexpr friend Derived operator/(Derived lhs, T rhs) { return lhs /= rhs; } // Intentional Matrix copy.
+			constexpr friend auto operator/(Matrix<RowSize, ColumnSize, T> lhs, T rhs) { return lhs /= rhs; }
+
+			// Intentional Matrix copy.
 
 			/// Takes the dot product of each row in \p lhs by each column in \p rhs to create a new matrix.
 			/// @param lhs A matrix with a number of columns equal to the number of rows in \p rhs.
 			/// @param rhs A matrix with a number of rows equal to the number of columns in \p lhs.
 			/// @return A matrix with the same number of rows as \p lhs and the same number of columns as \p rhs.
 			template <std::size_t OtherColumnSize>
-			constexpr friend Matrix<RowSize, OtherColumnSize, T> operator*(const Matrix<RowSize, ColumnSize, T>& lhs,
-			                                                               const Matrix<ColumnSize, OtherColumnSize, T>&
-			                                                               rhs)
+			constexpr friend auto operator*(const Matrix<RowSize, ColumnSize, T>& lhs,
+			                                const Matrix<ColumnSize, OtherColumnSize, T>&
+			                                rhs)
 			{
 				Matrix<RowSize, OtherColumnSize, T> result;
 				for (std::size_t row = 0; row < RowSize; ++row)
@@ -175,7 +182,8 @@ namespace Engine3
 			/// @param lhs A row vector with a number of columns equal to the number of rows in \p rhs.
 			/// @param rhs A matrix with a number of rows equal to the size of \p lhs.
 			/// @return A row vector with a size equal to the number of columns in \p rhs.
-			constexpr friend Vector<ColumnSize, T> operator*(const Vector<RowSize, T>& lhs, const Derived& rhs)
+			constexpr friend auto operator*(const Vector<RowSize, T>& lhs,
+			                                const Matrix<RowSize, ColumnSize, T>& rhs)
 			{
 				Vector<ColumnSize, T> rowVector;
 				for (std::size_t column = 0; column < rowVector.size(); ++column)
@@ -190,7 +198,8 @@ namespace Engine3
 			/// @param lhs A matrix with a number of columns equal to the size of \p rhs.
 			/// @param rhs A column vector with a number of rows equal to the number of columns in \p lhs.
 			/// @return A column vector with a size equal to the number of rows in \p lhs.
-			constexpr friend Vector<RowSize, T> operator*(const Derived& lhs, const Vector<ColumnSize, T>& rhs)
+			constexpr friend auto operator*(const Matrix<RowSize, ColumnSize, T>& lhs,
+			                                const Vector<ColumnSize, T>& rhs)
 			{
 				Vector<RowSize, T> columnVector;
 
@@ -203,14 +212,17 @@ namespace Engine3
 			}
 
 			/// Checks whether each element in the two matrices are equal.
-			friend bool operator==(const Derived& lhs, const Derived& rhs)
+			friend bool operator==(const Matrix<RowSize, ColumnSize, T>& lhs, const Matrix<RowSize, ColumnSize, T>& rhs)
 			{
 				for (std::size_t i = 0; i < lhs.size(); ++i) { if (lhs[i] != rhs[i]) { return false; } }
 				return true;
 			}
 
 			/// Checks whether any element in two matrices differ.
-			friend bool operator!=(const MatrixBase& lhs, const MatrixBase& rhs) { return !(lhs == rhs); };
+			friend bool operator!=(const Matrix<RowSize, ColumnSize, T>& lhs, const Matrix<RowSize, ColumnSize, T>& rhs)
+			{
+				return !(lhs == rhs);
+			};
 
 			// If these aren't deleted it makes it possible to perform comparisons like Matrix<4, 3>{} == Matrix<3, 4>{},
 			// as well as to arrays of the same type and size.
@@ -231,7 +243,7 @@ namespace Engine3
 	/// @tparam ColumnSize The horizontal size of the matrix.
 	/// @tparam T The type of each element stored in the matrix.
 	template <std::size_t RowSize, std::size_t ColumnSize = RowSize, Number T = float>
-	struct Matrix final : Detail::MatrixBase<RowSize, ColumnSize, T, Matrix<RowSize, ColumnSize, T>> {};
+	struct Matrix final : Detail::MatrixBase<RowSize, ColumnSize, T> {};
 
 	// Row first, then column to follow normal matrix conventions.
 	/// Matrix with its elements stored in row-major order.
@@ -239,7 +251,7 @@ namespace Engine3
 	/// @tparam ColumnSize The horizontal size of the matrix.
 	/// @tparam T The type of each element stored in the matrix.
 	template <Number T>
-	struct Matrix<2, 2, T> final : Detail::MatrixBase<2, 2, T, Matrix<2, 2, T>>
+	struct Matrix<2, 2, T> final : Detail::MatrixBase<2, 2, T>
 	{
 		/// Rotation matrix around the Z-Axis.
 		static constexpr Matrix Rotation(T radians) requires std::floating_point<T>
@@ -349,7 +361,7 @@ namespace Engine3
 	/// @tparam ColumnSize The horizontal size of the matrix.
 	/// @tparam T The type of each element stored in the matrix.
 	template <Number T>
-	struct Matrix<3, 3, T> final : Detail::MatrixBase<3, 3, T, Matrix<3, 3, T>>
+	struct Matrix<3, 3, T> final : Detail::MatrixBase<3, 3, T>
 	{
 		static constexpr Matrix RotationAboutX(T radians) requires std::floating_point<T>
 		{
@@ -539,7 +551,7 @@ namespace Engine3
 	/// @tparam ColumnSize The horizontal size of the matrix.
 	/// @tparam T The type of each element stored in the matrix.
 	template <Number T>
-	struct Matrix<4, 4, T> final : Detail::MatrixBase<4, 4, T, Matrix<4, 4, T>>
+	struct Matrix<4, 4, T> final : Detail::MatrixBase<4, 4, T>
 	{
 		static constexpr Matrix ScalingAlongCardinalAxes(T x, T y, T z, T w)
 		{
